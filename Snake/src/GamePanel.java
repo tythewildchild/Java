debb354c56f8c40b0adefa21eafa0a3ec5d006b0
Ty Hendershot <thendershot@solutionreach.com>
2022-04-05 00:03:59 -0600

    key listeners

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -80,7 +80,11 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkApple(){
-
+        if(x[0] == appleX && y[0] == appleY){
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
 
     public void checkCollision(){
@@ -92,9 +96,29 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public class MyKeyAdapter extends KeyAdapter{
-
         public void keyPresssed(KeyEvent e){
-
+            switch(e.getKeyCode()){
+                case KeyEvent.VK_LEFT:
+                    if(direction!='R'){
+                        direction = 'L';
+                    }
+                break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction!='L'){
+                        direction = 'R';
+                    }
+                break;
+                case KeyEvent.VK_UP:
+                    if(direction!='D'){
+                        direction = 'U';
+                    }
+                break;
+                case KeyEvent.VK_DOWN:
+                    if(direction!='U'){
+                        direction = 'D';
+                    }
+                break;
+            }
         }
     }
 
@@ -102,9 +126,40 @@ public class GamePanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(running){
             move();
-            checkCollision();
+            checkApple();
             checkCollision();
         }
         repaint();
     }
 }
+
+
+//    public void draw(Graphics g) {
+//        if(running) {
+//			for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+//				g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+//				g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+//			}
+//            g.setColor(Color.red);
+//            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+//            for(int i = 0; i< bodyParts;i++) {
+//                if(i == 0) {
+//                    g.setColor(Color.green);
+//                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+//                }
+//                else {
+//                    g.setColor(new Color(45,180,0));
+//                    //g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+//                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+//                }
+//            }
+//            g.setColor(Color.red);
+//            g.setFont( new Font("Ink Free",Font.BOLD, 40));
+//            FontMetrics metrics = getFontMetrics(g.getFont());
+//            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+//        }
+//        else {
+//            gameOver(g);
+//        }
+//    }
+
